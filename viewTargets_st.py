45e772d7c631132c52796f44fd01d1f44db3e9ff
rizMehdi <ccmeall@gmail.com>
2023-07-14 15:15:25 +0100

    Update viewTargets_st.py

--- viewTargets_st.py
+++ viewTargets_st.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 import pandas as pd
 st.set_page_config(page_title="PRIME",page_icon=None,layout="wide")
 
-st.title("Current Confirmed Survey Targets")
+st.title("PRIME: Current Confirmed Survey Targets")
 st.write("last revised on 14-Jul-23")
     # Fixed URL for the CSV file on GitHub
 csv_url_scotland = "https://raw.githubusercontent.com/thePrimeProject/survey-target-viewer/main/updatedTargets%20scotland_W6326%20-%20Prime%20Raw%20Data%20-%20V4%2B5%2B6.csv"
