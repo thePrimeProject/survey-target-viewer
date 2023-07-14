93bd1ec56aaf6a1d809863d3c67743beda15016b
rizMehdi <ccmeall@gmail.com>
2023-07-14 14:44:55 +0100

    new urls

--- viewTargets_st.py
+++ viewTargets_st.py
@@ -7,8 +7,8 @@ import pandas as pd
 st.title("Current Confirmed Targets")
 
     # Fixed URL for the CSV file on GitHub
-csv_url_scotland = "https://raw.githubusercontent.com/thePrimeProject/surveyTargetCalculation/main/updatedTargets%20scotland_W6326%20-%20Prime%20Raw%20Data%20-%20V4%2B5%2B6.csv?token=GHSAT0AAAAAACEWCQWE7JFMAEJ6IK3ZRXPMZFRJTVA"
-csv_url_england = "https://raw.githubusercontent.com/thePrimeProject/surveyTargetCalculation/main/updatedTargets%20england_W6326%20-%20Prime%20Raw%20Data%20-%20V4%2B5%2B6.csv?token=GHSAT0AAAAAACEWCQWFGGEXEXCC5TX3Z63OZFRJO3A"
+csv_url_scotland = "https://raw.githubusercontent.com/thePrimeProject/survey-target-viewer/main/updatedTargets%20scotland_W6326%20-%20Prime%20Raw%20Data%20-%20V4%2B5%2B6.csv"
+csv_url_england = "https://raw.githubusercontent.com/thePrimeProject/survey-target-viewer/main/updatedTargets%20england_W6326%20-%20Prime%20Raw%20Data%20-%20V4%2B5%2B6.csv"
 try:
     df_scot = pd.read_csv(csv_url_scotland)
     df_eng = pd.read_csv(csv_url_england)
