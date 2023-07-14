a1f0f6a59dfae7035f4efa60781ad4653fe2c703
rizMehdi <ccmeall@gmail.com>
2023-07-14 15:27:38 +0100

    fix indent

--- viewTargets_st.py
+++ viewTargets_st.py
@@ -10,6 +10,8 @@ st.write("last revised on 14-Jul-23")
     # Fixed URL for the CSV file on GitHub
 csv_url_scotland = "https://raw.githubusercontent.com/thePrimeProject/survey-target-viewer/main/updatedTargets%20scotland.csv"
 csv_url_england  = "https://raw.githubusercontent.com/thePrimeProject/survey-target-viewer/main/updatedTargets%20england.csv"
+
+try:   
     df_scot = pd.read_csv(csv_url_scotland)
     df_eng = pd.read_csv(csv_url_england)
     st.header("Scotland:")
